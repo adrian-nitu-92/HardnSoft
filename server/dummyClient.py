a7d823f85be3f496c9c2a53df5f8bedc24db59f0
Ioana Alexandra Antoche <ioana.antoche@yahoo.com>
2015-05-19 10:09:40 +0300

    refactoring

--- server/dummyClient.py
+++ server/dummyClient.py
@@ -2,7 +2,10 @@ import httplib
 import thread
 import time
 import threading
+import sys
 
+HOST_NAME = '192.168.1.168' # !!!REMEMBER TO CHANGE THIS!!!
+PORT_NUMBER = 9000 # Maybe set this to 9000.
 
 class MyThread(threading.Thread):
   def __init__(self, id):
@@ -15,25 +18,30 @@ class MyThread(threading.Thread):
     print "End Thread ",self.id
 
   def sendRequest(self):
-    print "ana are mere"
-    conn = httplib.HTTPConnection("192.168.1.168:9000")
+    conn = httplib.HTTPConnection(HOST_NAME + ":" + str(PORT_NUMBER))
     conn.request("GET","/putHeartRate?value="+str(self.id)+"&time="+str(time.time()))
     res = conn.getresponse()
     #print "request: ", self.id, " ---- ", res.status, res.reason
     conn.request("GET","/putNumSteps?value="+str(self.id)+"&time="+str(time.time()))
     res = conn.getresponse()
-    if self.id == 5 or self.id == 7:
-      conn.request("GET","/getChartsData")
-      res = conn.getresponse()
+    #if self.id == 5 or self.id == 7:
+    #  conn.request("GET","/getChartsData")
+    #  res = conn.getresponse()
     print "request: ", self.id, " ---- ", res.status, res.reason
 
+if __name__ == '__main__':
+  if len(sys.argv) == 2:
+    PORT_NUMBER = int(sys.argv[1])
+  if len(sys.argv) == 3:
+    HOST_NAME = sys.argv[1]
+    PORT_NUMBER = int(sys.argv[2])
 
-num = 0
-for i in xrange(10):
-  num += 1
-  t = MyThread(num)
-  t.start()
-  time.sleep(0.01)
+  num = 0
+  for i in xrange(10):
+    num += 1
+    t = MyThread(num)
+    t.start()
+    time.sleep(6)
 
 
 
